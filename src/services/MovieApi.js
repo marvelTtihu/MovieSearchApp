512ff8fa3f9586f940b6071d4c0b57da23372454
marvelTtihu <abangpung01@gmail.com>
2026-08-22 12:24:20 +0800

    feat: add movie API

--- src/services/MovieApi.js
+++ src/services/MovieApi.js
@@ -3,16 +3,13 @@ const API_URL = `http://omdbapi.com/`;
 
 export async function searchMovie(title) {
     const response = await fetch(`${API_URL}?apikey=${API_KEY}&s=${title}`);
-
     if(!response.ok){
         throw new Error("Failed to fetch.");
     }
-
     const data = await response.json();
 
     if(data.Response === "False"){
         throw new Error(data.Error);
     }
-
     return data.Search;
 }
